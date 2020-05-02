36f55bc09c17ceb2b0bb1ef072a2d54932ad10ee
diggledoot <39117755+diggledoot@users.noreply.github.com>
2020-05-02 15:46:43 +0800

    Change check_in and check_out to bigint due to int type having use until 2038

--- Kuching_Park_Hotel/app/src/main/java/com/example/kuching_park_hotel/Booking_History.java
+++ Kuching_Park_Hotel/app/src/main/java/com/example/kuching_park_hotel/Booking_History.java
@@ -1,22 +1,30 @@
 package com.example.kuching_park_hotel;
+
+import java.math.BigInteger;
+
 /*
 * Assumptions:
 * Integer: everything in int var below, I assume check_in and check_out is stored in UNIX time.
 * String: room_name
 * Float: total, the total price of that particular booking.
+*
+*
+* Correction 2/5/2020 3:44pm
+* check_in and check_out time changed to bigint data type because int has a limit until 2038
 * */
 public class Booking_History {
-    private int booking_id,room_id,guest_ref,check_in,check_out,room_qty;
+    private int booking_id,room_id,guest_ref,room_qty;
+    private BigInteger check_in,check_out;
     private String room_name;
     private float total;
 
-    public Booking_History(int booking_id, int room_id, int guest_ref, int check_in, int check_out, int room_qty, String room_name, float total) {
+    public Booking_History(int booking_id, int room_id, int guest_ref, int room_qty, BigInteger check_in, BigInteger check_out, String room_name, float total) {
         this.booking_id = booking_id;
         this.room_id = room_id;
         this.guest_ref = guest_ref;
+        this.room_qty = room_qty;
         this.check_in = check_in;
         this.check_out = check_out;
-        this.room_qty = room_qty;
         this.room_name = room_name;
         this.total = total;
     }
@@ -33,16 +41,16 @@ public class Booking_History {
         return guest_ref;
     }
 
-    public int getCheck_in() {
+    public int getRoom_qty() {
+        return room_qty;
+    }
+
+    public BigInteger getCheck_in() {
         return check_in;
     }
 
-    public int getCheck_out() {
+    public BigInteger getCheck_out() {
         return check_out;
-    }
-
-    public int getRoom_qty() {
-        return room_qty;
     }
 
     public String getRoom_name() {
