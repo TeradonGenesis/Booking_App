049b24f9e2dfe9d6cd0a5c1ae2210a697081c34d
diggledoot <39117755+diggledoot@users.noreply.github.com>
2020-05-03 20:32:41 +0800

    Implemented duplicate booking id check
    Online data against local data. Online takes priority.

--- Kuching_Park_Hotel/app/src/main/java/com/example/kuching_park_hotel/DatabaseHelper.java
+++ Kuching_Park_Hotel/app/src/main/java/com/example/kuching_park_hotel/DatabaseHelper.java
@@ -1,10 +1,16 @@
 package com.example.kuching_park_hotel;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "booking_history.db";
@@ -24,7 +30,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         SQLiteDatabase db = this.getWritableDatabase();
-
     }
 
     @Override
@@ -46,4 +51,52 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
-}
+    public Cursor getBookings(){
+        //Gets all the data from localdatabase
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT * FROM "+TABLE_NAME,null);
+    }
+
+    public void insertBookings(int booking_id, int room_id, int guest_ref,
+                               int room_qty, BigInteger check_in, BigInteger check_out, String room_name, float total){
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues payload = new ContentValues();
+
+        //These are the values we want to insert into the LOCAL database, hence
+        // titled booking_history.
+        payload.put(COLUMN_BOOKING_ID,booking_id);
+        payload.put(COLUMN_ROOM_ID,room_id);
+        payload.put(COLUMN_GUEST_REF,guest_ref);
+        payload.put(COLUMN_ROOM_QTY,room_qty);
+        payload.put(COLUMN_CHECK_IN,String.valueOf(check_in));
+        payload.put(COLUMN_CHECK_OUT,String.valueOf(check_out));
+        payload.put(COLUMN_ROOM_NAME,room_name);
+        payload.put(COLUMN_TOTAL,total);
+
+        db.insert(TABLE_NAME,null,payload);
+    }
+
+    //Check if online booking data exists in local db
+    //Accepts the online db data
+        public void checkDuplicate(ArrayList<Booking_History> bookings){
+            SQLiteDatabase db= getReadableDatabase();
+            Cursor data = db.rawQuery("SELECT booking_id FROM "+TABLE_NAME,null);
+            ArrayList<String> duplicates = new ArrayList<>();
+
+            if(data.moveToFirst()){
+                do{
+                    int booking_id = data.getInt(0);
+                    for(int i=0;i<bookings.size();i++){
+                        if(booking_id==bookings.get(i).getBooking_id()){
+                            duplicates.add(String.valueOf(bookings.get(i).getBooking_id()));
+                        }
+                    }
+                }while(data.moveToNext());
+            }
+
+            for(int i=0;i<duplicates.size();i++){
+                //Replace with code to return non-duplicate data, TODO
+                Log.d("console","Duplicate ID:"+duplicates.get(i));
+            }
+        }
+    }
