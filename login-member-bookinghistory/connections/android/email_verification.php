2d43d14b98af9eb2b7dd520fe07c0e75f19e3e02
choo98 <48203082+choo98@users.noreply.github.com>
2020-05-20 11:52:17 +0800

    Aldalton fixed the php file for login

--- login-member-bookinghistory/connections/android/email_verification.php
+++ login-member-bookinghistory/connections/android/email_verification.php
@@ -1,32 +1,32 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 $conn=mysqli_connect("localhost","root","","android_test");
 if(isset($_POST['user_email']))
 {
     $user_email = $_POST['user_email'];
-    $stmt = $conn->prepare("SELECT * FROM member WHERE user_email=?");
+    $stmt = $conn->prepare("SELECT * FROM member WHERE email=?");
+    echo $conn->error;
     $stmt->bind_param('s',$user_email);
     $stmt->execute();
     $result = $stmt->get_result();
     $arr = array();
     $error = array();
     
-    
-  
-    if(mysqli_num_rows($result) == 1)
+        
+    for($i=0;$i<mysqli_num_rows($result);$i++)
     {
-        for($i=0;$i<mysqli_num_rows($result);$i++)
-         {
-            $row = mysqli_fetch_assoc($result);
-            $arr[$i]=$row;
-         }
-         echo json_encode($arr);
-    }
-     else
+        $row = mysqli_fetch_assoc($result);
+        $arr[$i]=$row;
+     }
+    echo json_encode($arr);
+    
+   /*  else
     {
        array_push($error,"Error No Data Found");
         echo json_encode($error);
-    }
+    }*/
  
 }
 
