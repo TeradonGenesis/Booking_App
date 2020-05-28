0e17b77572a3114e25ec6e9ba0960852865a77a1
diggledoot <39117755+diggledoot@users.noreply.github.com>
2020-05-28 18:48:45 +0800

    Implemented storage of member object to be used in instant login given past successful login
    Pending token generation and validation

--- Kuching_Park_Hotel/app/src/main/java/com/example/kuching_park_hotel/Login.java
+++ Kuching_Park_Hotel/app/src/main/java/com/example/kuching_park_hotel/Login.java
@@ -17,6 +17,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -31,13 +32,16 @@ public class Login extends AppCompatActivity {
     EditText login_email;
     EditText login_password;
     String email_details;
+    String email_password;
     Member test_member;
 
-    //datebase
+    //RequestQueue - to be cleaned
     RequestQueue rq;
     private final String URL = "http://10.0.2.2/connections/android/email_verification.php";
-    private static final String user_email = "user_email";
+
+    //SharedPref - to be cleaned
     private static final String SHARED_PREF = "member";
+    private static final String MEMBER_OBJECT = "member_object";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,28 +49,40 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         //somewhere here check whether got previous login details using Shared Preferences
         //if not then do below login page
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF,MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString(MEMBER_OBJECT,"");
 
-        //Login stuff if don't have login details stored locally
-        button = findViewById(R.id.button_main);
-        login_email=findViewById(R.id.editText_login_email);
-        login_password=findViewById(R.id.editText_password_email);
+        //add code to check token validity
+        //then proceed to grab json object from shared pref
+        if(json.equals("")){
+            //Login stuff if don't have login details stored locally
+            button = findViewById(R.id.button_main);
+            login_email=findViewById(R.id.editText_login_email);
+            login_password=findViewById(R.id.editText_password_email);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-               //Toast.makeText(getApplicationContext(),"TEST", Toast.LENGTH_LONG).show();
-                //Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                //startActivity(intent);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    email_details = login_email.getText().toString();
+                    email_password = login_password.getText().toString();
+                    getData();
 
-                email_details = login_email.getText().toString();
-
-                getData();
-
-            }
-        });
-
+                }
+            });
+        }else{
+            test_member = gson.fromJson(json,Member.class);
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            intent.putExtra("Member",test_member);
+            startActivity(intent);
+        }
     }
 
+    //check token validity
+
+    //generate token and store in shared pref
+
+    //Get member data
     private void getData()
     {
         rq = MySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
@@ -98,7 +114,17 @@ public class Login extends AppCompatActivity {
                                             jsonObject.getInt("postcode"));
                             test_member = member;
                         }
-                        Log.d("Login, inside try catch",test_member.getName());
+                        //call genkey here before moving to new activity,save in shared pref
+
+                        //use gson to save object into shared pref
+                        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREF,MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        Gson gson = new Gson();
+                        String json = gson.toJson(test_member);
+                        editor.putString(MEMBER_OBJECT,json);
+                        editor.apply();
+
+                        //Start new activity
                         Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                         intent.putExtra("Member",test_member);
                         startActivity(intent);
@@ -118,7 +144,9 @@ public class Login extends AppCompatActivity {
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String,String> params = new HashMap<>();
                 String email = email_details;
+                String pwd = email_password;
                 params.put("user_email",email);
+                params.put("password",pwd);
                 return params;
             }
         };
