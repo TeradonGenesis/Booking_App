61544c62dabb2741510f90d463b5f627deb830dd
TeradonGenesis <48543482+TeradonGenesis@users.noreply.github.com>
2020-05-29 23:22:36 +0800

    LJL added finding room/booking form

---
+++ Kuching_Park_Hotel/app/src/main/java/com/example/kuching_park_hotel/FindRoom_Activity.java
@@ -0,0 +1,14 @@
+package com.example.kuching_park_hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FindRoom_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_room_);
+    }
+}
