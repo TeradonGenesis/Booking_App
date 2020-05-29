ddb93ce82478109f44057a22099038efb83d9e08
Marc <marcchai123@gmail.com>
2020-05-29 08:03:43 +0800

    mainpage
    rough UI design for main page

---
+++ Kuching_Park_Hotel/app/src/main/java/com/example/kuching_park_hotel/Mainpage_Fragment.java
@@ -0,0 +1,27 @@
+package com.example.kuching_park_hotel;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Mainpage_Fragment extends Fragment {
+
+    public Mainpage_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mainpage_, container, false);
+    }
+}
